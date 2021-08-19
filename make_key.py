5b850811d165992dab25a5dd3e1afe8b3daf676f
Gabriel Pery <gabrielperyy@gmail.com>
2021-08-19 15:53:59 +0100

    Update make_key.py

--- make_key.py
+++ make_key.py
@@ -1,14 +1,8 @@
 import os
-import sys
 import winreg as reg
 
 # Get path of current working directory and python.exe
 cwd = os.getcwd()
-python_exe = sys.executable
-
-# hide python terminal in windows
-hidden_terminal = '\\'.join(python_exe.split('\\')[:-1])+"\\pythonw.exe"
-
 
 # Set the path of the context menu (right-click menu)
 key_path = r'Directory\\Background\\shell\\Unzipper\\'
@@ -19,5 +13,5 @@ reg.SetValue(key, '', reg.REG_SZ, '&Unzip all zip files')
 
 # create inner key
 key1 = reg.CreateKey(key, r"command")
-reg.SetValue(key1, '', reg.REG_SZ, python_exe + f' "{cwd}\\Unzipper.py"')
-reg.SetValue(key1, '', reg.REG_SZ, hidden_terminal + f' "{cwd}\\Unzipper.py"')  # hides terminal
+reg.SetValue(key1, '', reg.REG_SZ, f'{cwd}\\Unzipper.exe')
+reg.SetValue(key1, '', reg.REG_SZ, f'{cwd}\\Unzipper.exe')  # hides terminal
