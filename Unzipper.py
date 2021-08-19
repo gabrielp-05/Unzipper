30c0916e03947aac6d9aa5648deb4252a9427a25
Gabriel Pery <gabrielperyy@gmail.com>
2021-08-19 21:28:08 +0100

    Update Unzipper.py

--- Unzipper.py
+++ Unzipper.py
@@ -1,5 +1,5 @@
-import os, logging, ctypes
-from zipfile import ZipFile
+import os, sys, ctypes, logging
+from pyunpack import Archive
 from glob import glob
 
 logging.basicConfig(filename=f'C:\\Users\\{os.getlogin()}\\AppData\\Local\\Unzipper\\errorlog.log', level=logging.ERROR, force=True, format='%(asctime)s %(levelname)s %(name)s %(message)s')
@@ -10,7 +10,7 @@ def main():
     """
 
     cwd = os.getcwd()
-    fileTypes = ['.zip'] # will later include support for '.rar','.7z' filetypes
+    fileTypes = ['.zip','.rar','.7z']
     zipFiles = list()
     for fileType in fileTypes:
         zipFiles.extend(glob(cwd+r'\\*{}'.format(fileType)))
@@ -21,9 +21,8 @@ def main():
     for file in zipFiles:
         fileName = os.path.basename(file)
         try:
-            with ZipFile(file,'r') as zipObj:
-                zipObj.extractall(cwd+r'\\Unzipped\\'+os.path.splitext(fileName)[0])
-                filesExtracted += 1
+            Archive(file).extractall(cwd+r'\\Unzipped\\'+os.path.splitext(fileName)[0], auto_create_dir=True)
+            filesExtracted += 1
         except Exception as e:
             logging.error(e)
             error = True
