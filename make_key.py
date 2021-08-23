41f8112b75d82dd4c24dd9b308d61fbd48b2d672
Gabriel Pery <gabrielperyy@gmail.com>
2021-08-23 20:22:47 +0100

    Adds the version name of the program to the Registry

--- make_key.py
+++ make_key.py
@@ -1,17 +1,22 @@
 import os
 import winreg as reg
+import glob
 
 # Get path of current working directory and python.exe
 cwd = os.getcwd()
 
+# Get path of main exe to be added to context menu (prioritises the first in alphabetical order)
+exe = glob.glob(f"{cwd}\\Unzipper*.exe")[0]
+filename = os.path.splitext(os.path.basename(exe))[0]
+
 # Set the path of the context menu (right-click menu)
-key_path = r'Directory\\Background\\shell\\Unzipper\\'
+key_path = f'Directory\\Background\\shell\\{filename}\\'
 
 # Create outer key
 key = reg.CreateKey(reg.HKEY_CLASSES_ROOT, key_path)
-reg.SetValue(key, '', reg.REG_SZ, '&Unzip all zip files')
+reg.SetValue(key, '', reg.REG_SZ, '&Uncompress all compressed files')
 
 # create inner key
 key1 = reg.CreateKey(key, r"command")
-reg.SetValue(key1, '', reg.REG_SZ, f'{cwd}\\Unzipper.exe')
-reg.SetValue(key1, '', reg.REG_SZ, f'{cwd}\\Unzipper.exe')  # hides terminal
+reg.SetValue(key1, '', reg.REG_SZ, exe)
+reg.SetValue(key1, '', reg.REG_SZ, exe)  # hides terminal
